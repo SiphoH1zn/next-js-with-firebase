078531526942325dcf546d61d3030fa900c87ac3
Andile <aa.chonco@gmail.com>
2026-03-14 18:16:52 +0200

    enable CORS

--- dist/main.js
+++ dist/main.js
@@ -5,10 +5,15 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    });
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, transform: true }));
     const port = process.env.PORT || 3000;
     await app.listen(port);
-    console.log('Application running on http://localhost:3000');
+    console.log('Application running on ${port}');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true,transform: true  }));
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log('Application running on http://localhost:3000');
+  console.log('Application running on ${port}');
 }
 bootstrap();
